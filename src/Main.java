96050302ac5702b8ca45969bb44b5a05434837d7
asrodionov <arodionov27@yandex.ru>
2020-05-04 16:44:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketPrice = 10000;
+        int CostPerMile = 20;
+        int AmountMiles = TicketPrice / CostPerMile;
+        System.out.println(AmountMiles);
+    }
+}
